325c0e8b8af801a13db316f2c8741b6b3bc77bfd
servosdesign <servos.design0@gmail.com>
2021-12-26 18:10:57 -0600

    sing in page component, button components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header-component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/shop" element={<ShopPage/>} />
+        <Route path="/signin" element={<SignInAndSignUpPage/>} />
       </Routes>
     </div>
   );
