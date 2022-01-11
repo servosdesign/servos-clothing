5af28dbc253ca6297de4384c1b78449d9e44da4b
servosdesign <servos.design0@gmail.com>
2022-01-11 17:41:08 -0600

    created selectors and updated corresponding components with redux flow for directoy/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
 const CollectionPage = () => {
-  let { collectionId } = useParams();
+  const { collectionId } = useParams();
+  const collection = useSelector(selectCollection(collectionId));
+  const { title, items } = collection;
   return (  
   <div className='collection-page'>
-    <h2>Category Page</h2>
+    <h2 className='title'>{ title }</h2>
+    <div className='items'>
+      {items.map(item => (<CollectionItem key={item.id} item={item} />))}
+    </div>
   </div>
-)};
+  );
+};
 
 export default CollectionPage;
