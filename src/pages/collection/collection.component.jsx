001d452653f69abbb4de9a5dee4e771beba96428
servosdesign <servos.design0@gmail.com>
2022-01-20 23:19:46 -0600

    shop page data linked to backend

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+ 
+import './collection.styles.scss';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
-
+ 
 const CollectionPage = () => {
-  const { collectionId } = useParams();
-  const collection = useSelector(selectCollection(collectionId));
+  let params = useParams();
+  const collection = useSelector(selectCollection(params.collectionId));
   const { title, items } = collection;
-  return (  
-  <div className='collection-page'>
-    <h2 className='title'>{ title }</h2>
-    <div className='items'>
-      {items.map(item => (<CollectionItem key={item.id} item={item} />))}
+ 
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-  </div>
   );
 };
-
+ 
 export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,35 +7,47 @@ import { connect } from 'react-redux';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
  
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.get().then((snapshot) => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
-  render(){
+  render() {
+    const { loading } = this.state;
+ 
     return (
       <div className="shop-page">
-      <Routes>
-        <Route path="/" element={<CollectionsOverview />} />
-        <Route path=":collectionId" element={<CollectionPage />} />
-      </Routes>
-    </div>
-    ) 
+        <Routes>
+          <Route path="/" element={<CollectionsOverviewWithSpinner isLoading={loading}{...this.props}/>
+          }/>
+          <Route path=":collectionId" element={<CollectionPageWithSpinner isLoading={loading} {...this.props} />}/>
+        </Routes>
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
