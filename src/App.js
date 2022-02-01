1f4c0c2563a17c0ea985f70c1fb3af952adfa575
servosdesign <servos.design0@gmail.com>
2022-02-01 17:32:02 -0600

    updated to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -14,33 +14,24 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  };
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  };
+const App = ({ checkUserSession, currentUser }) => {
+ useEffect(() => {
+  checkUserSession()
+ }, [checkUserSession]);
   
-  render() {
-    return (
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="shop/*" element={<ShopPage />} />
-          <Route exact path="/checkout" element={<CheckoutPage/>} />
-          <Route exact path="/signin" element={
-            this.props.currentUser ? <Navigate to="/" /> : <SignInAndSignUpPage />
-          }/>
-        </Routes>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="shop/*" element={<ShopPage />} />
+        <Route exact path="/checkout" element={<CheckoutPage/>} />
+        <Route exact path="/signin" element={
+          currentUser ? <Navigate to="/" /> : <SignInAndSignUpPage />
+        }/>
+      </Routes>
+    </div>
+  );
 };
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
  
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
@@ -7,26 +7,22 @@ import CollectionPageContainer from '../collection/collection.container';
 import { connect } from 'react-redux';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, props }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-
-    return (
-      <div className="shop-page">
-        <Routes>
-          <Route path="/" element={<CollectionsOverviewContainer {...this.props}/>
-          }/>
-          <Route path=":collectionId" element={<CollectionPageContainer  {...this.props} />}/>
-        </Routes>
-      </div>
-    );
-  }
+  return (
+    <div className="shop-page">
+      <Routes>
+        <Route path="/" element={<CollectionsOverviewContainer {...props}/>
+        }/>
+        <Route path=":collectionId" element={<CollectionPageContainer  {...props} />}/>
+      </Routes>
+    </div>
+  );
 };
+
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
